39e711893cd0fb4c92543a23e70954992d4ca821
Borja Escolano <escolanomartinborja@gmail.com>
2025-07-07 17:25:34 +0200

    cambios básicos

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,9 @@
-// vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// Simula __dirname
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -15,5 +13,12 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    host: '0.0.0.0', // <- Importante para acceder desde fuera (como ngrok o red local)
+    port: 5173,      // <- O el puerto que uses
+    strictPort: true,
+    allowedHosts: ['all'], // <- Permitir todos los hosts (o puedes poner manualmente el subdominio de ngrok)
+    origin: 'https://e963-95-62-80-190.ngrok-free.app', // <- A veces Vite también valida esto
   },
 })
